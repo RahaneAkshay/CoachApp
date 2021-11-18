4eb6aa09a78468e58e90471bde8502316bee9e7b
Akshay Rahane <akshay.rahane@perennialsys.com>
2021-11-18 11:47:10 +0530

    Component register in routes

--- src/router.js
+++ src/router.js
@@ -1,4 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachList from './pages/coaches/CoachList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/request/ContactCoach.vue';
+import RequestRecived from './pages/request/RequestRecived.vue';
+import NotFound from './pages/NotFound.vue';
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -6,12 +12,13 @@ export const router = createRouter({
     { path: '', redirect: '/coaches' },
     {
       path: '/coaches',
-      componet: null,
-      children: [{ path: '/contact', componet: null }],
+      componet: CoachList,
+
+      children: [{ path: '/contact', componet: ContactCoach }],
     },
-    {path:'/register',componet:null},
-    {path:'/request',componet:null},
-    {path:'/request',componet:null},
-    {path:'/:notFound(.*)',componet:null},
+    { path: '/coaches/:id', componet: CoachDetails },
+    { path: '/register', componet: CoachRegistration },
+    { path: '/request', componet: RequestRecived },
+    { path: '/:notFound(.*)', componet: NotFound },
   ],
 });
